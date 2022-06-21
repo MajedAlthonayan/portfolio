38a9f13008cf2970573e538d248d47294efb5962
Majed Althonayan <97466204+MajedAlthonayan@users.noreply.github.com>
2022-06-21 17:49:16 +0100

    added text to homepage.
    stopped at 33 min

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,11 +2,16 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout'
+import Home from './Components/Home';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<Home />}></Route>
+      </Route>
+      
+      
 
     </Routes>
   );
